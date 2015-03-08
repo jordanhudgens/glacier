45b9a76d4f6074e8a9d38479f6e5c2fafd551930
Jordan Hudgens <jordan@wow.com>
2015-03-07 20:40:50 -0600

    Built out the initial feature generator

--- runner.rb
+++ runner.rb
@@ -4,13 +4,29 @@ class Runner
   def initialize
   end
 
-  def self.application_generator(application_name, language)
+  def application_generator(application_name, language)
 
   end
 
 
   def feature_generator(feature_name, language)
-    feature_path = "#{name_cleaner(feature_name)}/model.#{language_extension_generator(language.downcase)}"
+    model_path = "#{name_cleaner(feature_name)}/model.#{language_extension_generator(language.downcase)}"
+    controller_path = "#{name_cleaner(feature_name)}/controller.#{language_extension_generator(language.downcase)}"
+    views_path = "#{name_cleaner(feature_name)}/views/index.html"
+    routes_path = "#{name_cleaner(feature_name)}/routes.#{language_extension_generator(language.downcase)}"
+
+    paths = [model_path, controller_path, views_path, routes_path]
+
+    paths.each do |path|
+      dirname = File.dirname(path)
+
+      unless File.directory?(dirname)
+        FileUtils.mkdir_p(dirname)
+      end
+
+      File.open(path, 'w+') { |f| f.write("write your stuff here")  }
+    end
+
     
   end
 
