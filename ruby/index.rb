e7f04b54fa1e59c4305401210644dfeeda1326eb
Jordan Hudgens <jordan@wow.com>
2015-03-09 19:27:23 -0500

    Setup the application directory structure

--- ruby/index.rb
+++ ruby/index.rb
@@ -1,7 +1,7 @@
 require 'webrick'
 server = WEBrick::HTTPServer.new(:Port => 8080)
 server.mount_proc('/') {|request, response| response.body = File.open("sample.html.erb")}
-server.mount_proc('/coolstory') {|request, response| response.body = File.open("coolstory.html.erb")}
-server.mount_proc('/yay') {|request, response| response.body = File.open("yay.html.erb")}
+# server.mount_proc('/coolstory') {|request, response| response.body = File.open("coolstory.html.erb")}
+# server.mount_proc('/yay') {|request, response| response.body = File.open("yay.html.erb")}
 trap("INT") {server.shutdown}
 server.start
