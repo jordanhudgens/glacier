39048f1a2b210ba8785443402398bf6802358085
Jordan Hudgens <jordan@wow.com>
2015-04-01 09:29:31 -0500

    Customized launch page

--- assets/asset_generator.rb
+++
@@ -1,5 +0,0 @@
-module AssetGenerator
-  def install_bootsrap
-    # TODO copy css/fonts/js from assets directory to new assets directory in the newly created application
-  end
-end
--- setup/application.rb
+++
@@ -1,5 +0,0 @@
-require 'webrick'
-server = WEBrick::HTTPServer.new(:Port => 8080)
-server.mount_proc('/') {|request, response| response.body = File.open('launch/get_started.html')}
-trap('INT') {server.shutdown}
-server.start
